94841f455aeefd6c100dce2a0dba8e312aab5319
Sheldon Lee <liwentao0523@hotmail.com>
2021-11-12 20:46:09 +0800

    MNIST finish

--- dataset/NMIST/PickImg.py
+++ dataset/NMIST/PickImg.py
@@ -1,14 +1,16 @@
 import os
+import shutil
 
 if __name__ == '__main__':
-    dataset_path = "D:\\Dataset\\NMIST\\train1_images\\"
-    g = os.walk(r"D:\\Dataset\\NMIST\\train1_images\\")
+    dataset_path = "D:\\Dataset\\NMIST\\test1_images\\"
+    target_path = "D:\\Dataset\\NMIST\\test_images\\"
+    g = os.walk(r"D:\\Dataset\\NMIST\\test1_images\\")
     for path, dir_list, file_list in g:
-        print(dir_list)
         count = 0
         for file_name in file_list:
-
+            target_path = path.replace("test1", "test")
+            shutil.copyfile(path+"\\"+file_name, target_path+"\\"+str(count)+".jpg")
+            print(target_path+"\\"+str(count)+".jpg")
             count += 1
-            if count >= 1:
+            if count >= 34:
                 break
-
