e37ca39982f81c9565ad9673ab25fc5664048939
Sheldon Lee <liwentao0523@hotmail.com>
2021-11-13 21:10:52 +0800

    V1.5

--- train_DL/train.py
+++ train_DL/train.py
@@ -16,19 +16,19 @@ def parse_example(record):
     w = tf.cast(dict_data['width'], tf.int64)
     h = tf.cast(dict_data['height'], tf.int64)
     img = tf.io.decode_raw(dict_data['image'], tf.uint8)
-    img = tf.cast(tf.reshape(img, [w, h, 3]), dtype=tf.float32)
+    img = tf.cast(tf.reshape(img, [w, h, 1]), dtype=tf.float32)
     return {'image': img, 'label': label, 'width': w, 'height': h}
 
 
 def load_ds():
-    input_files = ['D:\\WorkSpace\\PycharmProjects\\ML_2021\\dataset\\train.tfrecord']
+    input_files = ['D:\\WorkSpace\\PycharmProjects\\ML_2021\\dataset\\TFrecord\\train.tfrecord']
     ds = tf.data.TFRecordDataset(input_files)
     ds = ds.map(parse_example)
     return ds
 
 
 def load_val_ds():
-    input_files = ['D:\\WorkSpace\\PycharmProjects\\ML_2021\\dataset\\test.tfrecord']
+    input_files = ['D:\\WorkSpace\\PycharmProjects\\ML_2021\\dataset\\TFrecord\\test.tfrecord']
     ds = tf.data.TFRecordDataset(input_files)
     ds = ds.map(parse_example)
     return ds
@@ -61,58 +61,61 @@ if __name__ == '__main__':
     num_classes = len(all_characters)
     print('all characters: {}'.format(num_classes))
     train_dataset = load_ds()
-    train_dataset = train_dataset.shuffle(640).map(preprocess).batch(64).repeat()
+    train_dataset = train_dataset.map(preprocess)
+    train_dataset = train_dataset.shuffle(640)
+    train_dataset = train_dataset.batch(64)
+    train_dataset = train_dataset.repeat()
     val_ds = load_val_ds()
-    val_ds = val_ds.shuffle(640).map(preprocess).batch(64).repeat()
-    # for data in val_ds.take(2):
-    #     print(data)
+    val_ds = val_ds.map(preprocess)
+    val_ds = val_ds.shuffle(640)
+    val_ds = val_ds.batch(64)
+    val_ds = val_ds.repeat()
+
     model = keras.models.Sequential([
-        keras.layers.Conv2D(input_shape=(64, 64, 3), filters=32, kernel_size=(3, 3), strides=(1, 1),
-                            padding='same', activation='relu'),
-        keras.layers.MaxPool2D(pool_size=(2, 2), padding='same'),
-        keras.layers.Conv2D(filters=64, kernel_size=(3, 3), padding='same'),
-        keras.layers.Dropout(0.25),
-        keras.layers.MaxPool2D(pool_size=(2, 2), padding='same'),
+        keras.layers.Conv2D(32, (3, 3), padding="valid", strides=(1, 1), data_format="channels_last", input_shape=(64, 64, 1), activation="relu"),
+        keras.layers.MaxPool2D(pool_size=(2, 2)),
+        keras.layers.Dropout(0.2),
+        keras.layers.Conv2D(64, (3, 3), padding="valid", strides=(1, 1), data_format="channels_last", activation="relu"),
+        keras.layers.MaxPool2D(pool_size=(2, 2)),
+        keras.layers.Dropout(0.2),
         keras.layers.Flatten(),
-        keras.layers.Dense(3756, activation='softmax')
-        # keras.layers.Conv2D(64, kernel_size=3, padding="same", activation="relu"),
-        # keras.layers.Conv2D(128, kernel_size=3, padding="same", activation="relu"),
-        # keras.layers.MaxPool2D(),
-        # keras.layers.Flatten(),
-        # keras.layers.Dropout(0.25),
-        # keras.layers.Dense(128, activation="relu"),
-        # keras.layers.Dropout(0.5),
-        # keras.layers.Dense(3756, activation="softmax")
+        keras.layers.Dense(1024, activation='relu'),
+        keras.layers.Dropout(0.2),
+        keras.layers.Dense(3765, activation='softmax')
     ])
-    model.compile(loss="sparse_categorical_crossentropy",
-                  optimizer="nadam",
-                  metrics=["accuracy"])
     start_epoch = 0
     ckpt_path = "D:\\WorkSpace\\PycharmProjects\\ML_2021\\checkpoints\\"
     latest_ckpt = tf.train.latest_checkpoint(os.path.dirname(ckpt_path))
+    model.save_weights(ckpt_path.format(epoch=0))
+    model.save(os.path.join(os.path.dirname(ckpt_path), 'cn_ocr.h5'))
     if latest_ckpt:
         start_epoch = int(latest_ckpt.split('-')[1].split('.')[0])
         model.load_weights(latest_ckpt)
         print('model resumed from: {}, start at epoch: {}'.format(latest_ckpt, start_epoch))
     else:
         print('passing resume since weights not there. training from scratch')
-    callbacks = [tf.keras.callbacks.ModelCheckpoint(ckpt_path, save_weights_only=True, verbose=1, period=500),
+    callbacks = [tf.keras.callbacks.ModelCheckpoint(ckpt_path, save_weights_only=True, verbose=1, period=100),
                  tf.keras.callbacks.TensorBoard(run_logdir)]
+    learning_rate = 0.01
+    decay = 1e-6
+    momentum = 0.8
+    nesterov = True
+    sgd_optimizer = tf.keras.optimizers.SGD(lr=learning_rate, decay=decay,
+                                            momentum=momentum, nesterov=nesterov)
     model.compile(
-        optimizer=tf.keras.optimizers.Adam(),
-        loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+        optimizer=sgd_optimizer,
+        loss="sparse_categorical_crossentropy",
         metrics=['accuracy'])
+    model.summary()
     try:
         tf.data.experimental.ignore_errors()
         model.fit(
             train_dataset,
             validation_data=val_ds,
-            validation_steps=1000,
+            validation_steps=100,
             epochs=15000,
             steps_per_epoch=1024,
             callbacks=callbacks)
     except KeyboardInterrupt:
         model.save_weights(ckpt_path.format(epoch=0))
         print('keras model saved.')
-    model.save_weights(ckpt_path.format(epoch=0))
-    model.save(os.path.join(os.path.dirname(ckpt_path), 'cn_ocr.h5'))
